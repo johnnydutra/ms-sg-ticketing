b188a58ff5aa14cb30ad546d493bf9be8dc26761
Johnny Dutra <johnny.dutra@gmail.com>
2024-07-01 19:04:23 -0300

    through section 7

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
 
 import { currentUserRouter } from './routes/current-user';
 import { signInRouter } from './routes/sign-in';
 import { signOutRouter } from './routes/sign-out';
 import { signUpRouter } from './routes/sign-up';
+import { errorHandler } from './middlewares/error-handler';
+
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
@@ -14,6 +18,12 @@ app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
 
+app.all('*', async (req, res, next) => {
+  next(new NotFoundError());
+});
+
+app.use(errorHandler);
+
 app.listen(3000, () => {
-  console.log('Listening on port 3000!');
+  console.log('Listening on port 3000');
 });
