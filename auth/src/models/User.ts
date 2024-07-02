067fb066653e2ca56b33d2514a5ff291c4a1993b
Johnny Dutra <johnny.dutra@gmail.com>
2024-07-02 15:26:30 -0300

    adding password hashing

--- auth/src/models/User.ts
+++ auth/src/models/User.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Password } from '../services/Password';
 
 interface UserAttrs {
   email: string;
@@ -28,6 +29,14 @@ const userSchema = new mongoose.Schema({
 userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
+
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
+});
 
 const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
 
