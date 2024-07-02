e3ff77831df2e2aec31fab19363f3f1105deb45a
Johnny Dutra <johnny.dutra@gmail.com>
2024-07-02 15:10:39 -0300

    adding existing email error handling

--- auth/src/routes/sign-up.ts
+++ auth/src/routes/sign-up.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { User } from '../models/User';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -20,10 +21,19 @@ router.post(
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
-    console.log('Creating an user...');
-    throw new DatabaseConnectionError();
 
-    res.send({});
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      throw new BadRequestError('Email already in use');
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
